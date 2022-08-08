5f2467f8d65fdce6a6f335c5ff4065dad060cacd
Xiaoyan Wang <horizonblueblueblue@gmail.com>
2022-08-07 22:35:56 -0700

    Update left hand keymap

--- keyboards/handwired/dactyl_manuform/5x7/keymaps/right/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x7/keymaps/right/keymap.c
@@ -11,7 +11,7 @@
 // Some basic macros
 #define TASK   LCTL(LSFT(KC_ESC))
 #define TAB_R  LCTL(KC_TAB)
-#define TAB_L  LCTL(LSFT(KC_TAB))
+#define TAB_L  LCTL(LSFT(KC_TAB)) 
 #define TAB_RO LCTL(LSFT(KC_T))
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -20,11 +20,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,    KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,
         KC_TAB,    KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_LBRC,
         KC_LCTL,   KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   TAB_RO,
-        OSM(MOD_LSFT),   KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,
+        KC_LSFT,   KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,
         KC_CAPS,   KC_LGUI, TAB_L,   TAB_R,
-                                    TT(_FN), KC_SPC,
-                                    KC_END, KC_HOME,
-                                    KC_PSCR, TASK,
+                                    KC_LALT, KC_SPC,
+                                    KC_LGUI, KC_HOME,
+                                    TT(_FN), KC_END,
         // right hand
                           KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_GRV,
                           KC_RBRC, KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_BSLS,
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                             KC_LEFT, KC_UP,    KC_DOWN,  KC_RGHT,
              KC_BSPC, KC_ENT,
              KC_PGUP, KC_PGDN,
-             KC_LCTL, KC_LALT
+             KC_LCTL, TT(_FN)
     ),
 
     [_FN] = LAYOUT_5x7(
