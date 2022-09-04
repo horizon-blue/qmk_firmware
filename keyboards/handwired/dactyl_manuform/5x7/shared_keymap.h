ab287f371509865699e5f7128b0ae4291df773a8
Xiaoyan Wang <horizonblueblueblue@gmail.com>
2022-09-04 16:50:57 -0700

    Update keymap

--- keyboards/handwired/dactyl_manuform/5x7/shared_keymap.h
+++ keyboards/handwired/dactyl_manuform/5x7/shared_keymap.h
@@ -18,20 +18,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_ESC,    KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,
         KC_TAB,    KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_LBRC,
-        KC_LCTL,   KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   QK_BOOTLOADER,
+        KC_LCTL,   KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_BTN2,
         KC_LSFT,   KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,
-        KC_CAPS,   KC_LGUI, TAB_L,   TAB_R,
-                                    KC_LALT, KC_SPC,
-                                    KC_LGUI, KC_HOME,
+        KC_CAPS,   KC_LALT, KC_PGUP, KC_PGDN,
+                                    KC_LGUI, KC_SPC,
+                                    KC_DEL,  KC_HOME,
                                     TT(_FN), KC_END,
         // right hand
-                          KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,
-                          KC_RBRC, KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_BSLS,
-                    QK_BOOTLOADER, KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
-                                   KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_UP,  OSM(MOD_RSFT),
+                          KC_7,     KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,
+                          KC_RBRC,  KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_BSLS,
+                          KC_BTN1,  KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
+                                    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_UP,  TT(_FN),
                                             KC_SLSH, KC_LEFT,    KC_DOWN,  KC_RGHT,
                 KC_ENTER,
-                TT(_FN),
+                KC_SPC,
         KC_PGUP, KC_PGDN
     ),
 
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // right hand
                           KC_F7,     KC_F8,     KC_F9,     KC_F10,    KC_F11,    KC_F12,    _______,
                           _______,   _______,   _______,   _______,   _______,   _______,   _______,
-                          _______,   _______,   _______,   _______,   _______,   _______,   _______,
+                          QK_BOOT,   _______,   _______,   _______,   _______,   _______,   _______,
                                      _______,   _______,   _______,   _______,   _______,   _______,
                                                            _______,   _______,   _______,   _______,
                       _______,
