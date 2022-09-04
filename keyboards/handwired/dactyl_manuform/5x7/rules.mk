fd868ee1e4ce839bf4b2119205df04c540486262
Xiaoyan Wang <horizonblueblueblue@gmail.com>
2022-09-04 15:41:39 -0700

    Disable bluetooth (for now)

--- keyboards/handwired/dactyl_manuform/5x7/rules.mk
+++ keyboards/handwired/dactyl_manuform/5x7/rules.mk
@@ -17,3 +17,7 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 SPLIT_KEYBOARD = yes
+
+# For bluetooth
+BLUETOOTH_ENABLE = no
+BLUETOOTH_DRIVER = BluefruitLE
