491fbfe252477de10bf61750bd93becfb3f7021e
Xiaoyan Wang <horizonblueblueblue@gmail.com>
2022-09-11 23:23:40 -0700

    Update keymap for the new dactyl build

--- keyboards/handwired/dactyl_manuform/5x7/shared_keymap.h
+++ keyboards/handwired/dactyl_manuform/5x7/shared_keymap.h
@@ -23,12 +23,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_ESC,    KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,
         KC_TAB,    KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_LBRC,
-        KC_LCTL,   KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   MO(_MOUSE),
+        KC_LCTL,   KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_GRV,
         KC_LSFT,   KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,
-        KC_CAPS,   KC_LALT, KC_PGUP, KC_PGDN,
-                                    KC_LGUI, KC_SPC,
-                                    KC_DEL,  KC_HOME,
-                                    TT(_FN), KC_END,
+        TT(_FN),   KC_LALT, KC_PGUP, KC_PGDN,
+                                    KC_LGUI, KC_SPC, KC_DEL,
+                                    TT(_FN), KC_HOME,KC_END,
         // right hand
                           KC_7,       KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,
                           KC_RBRC,    KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_BSLS,
@@ -43,12 +42,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_FN] = LAYOUT_5x7(
         // left hand
         _______,   KC_F1,     KC_F2,      KC_F3,    KC_F4,     KC_F5,    KC_F6,
-        _______,   _______,   _______,    KC_UP,    _______,   _______,  _______,
-        KC_VOLD,   KC_VOLU,   KC_LEFT,    KC_DOWN,  KC_RGHT,   _______,  QK_BOOT,
+        _______,   _______,   _______,   _______,    _______,   _______,  _______,
+        KC_CAPS,   KC_VOLD,   KC_VOLU,   _______,   _______,  _______,  QK_BOOT,
         _______,   _______,   _______,   _______,   _______,   _______,
-        KC_MSTP,   KC_MPLY,   KC_MPRV,   KC_MNXT,
+        _______,   _______,   _______,   _______,
                                     _______, _______,
-                                    _______, _______,
+                                    KC_DEL,  _______,
                                     _______, _______,
         // right hand
                           KC_F7,     KC_F8,     KC_F9,     KC_F10,    KC_F11,    KC_F12,    _______,
