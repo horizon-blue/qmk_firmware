4d97c9eae978032f4349aeba563f31c9742811c4
Xiaoyan Wang <horizonblueblueblue@gmail.com>
2022-09-23 23:24:02 -0700

    Enable VIA

--- keyboards/handwired/dactyl_manuform/5x7/rules.mk
+++ keyboards/handwired/dactyl_manuform/5x7/rules.mk
@@ -17,6 +17,7 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 SPLIT_KEYBOARD = yes
+VIA_ENABLE = yes
 
 # For bluetooth
 BLUETOOTH_ENABLE = no
